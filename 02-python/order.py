7342553457237a7cf4244f4df6fdad0d462a770a
Tejdeep <tejdeep826@gmail.com>
2026-08-21 01:15:14 +0530

    Added order.py file

---
+++ 02-python/order.py
@@ -0,0 +1,12 @@
+customer_name = "Rahul"
+product = "Laptop"
+price = 50000
+quantity = 2
+
+total_amount = price * quantity
+
+print("Customer:", customer_name)
+print("Product:", product)
+print("Price:", price)
+print("Quantity:", quantity)
+print("Total amount:", total_amount)

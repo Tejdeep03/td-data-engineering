c391de40aff8a2081ee4c13712eeb930908c20b5
Tejdeep <tejdeep826@gmail.com>
2026-08-21 01:15:57 +0530

    Added order_processor.py file

---
+++ 02-python/order_processor.py
@@ -0,0 +1,18 @@
+
+
+customer_name="Anu"
+Product="Haedphones"
+Price=2500
+Quantity=3
+Discount=10/100
+
+
+subtotal = Price * Quantity
+discount_amount = subtotal * discount /100
+final_amount = subtotal - discount_amount
+
+print("Customer:", customer_name)
+print("Product:", Product)
+print("Subtotal:", subtotal)
+print("Discount Amount:", discount_amount)
+print("Final Amount:", final_amount)

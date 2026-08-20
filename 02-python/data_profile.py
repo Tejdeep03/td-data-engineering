9c6cc90d7de171eb0d8983d3befda1f09b8acc20
Tejdeep <tejdeep826@gmail.com>
2026-08-21 00:55:30 +0530

    Add python data profile practise

---
+++ 02-python/data_profile.py
@@ -0,0 +1,11 @@
+name ="Tejdeep"
+age=22
+course="MCA"
+technologies=5
+target_role="Associate Data Engineer"
+
+print("My name is",name)
+print("My age is",age)
+print("I am pursuing",course)
+print("I have experience in",technologies)
+print("My target role is",target_role)

1da1094f86f6b5a481ac4884cdea666c03c8e8c3
Tejdeep <tejdeep826@gmail.com>
2026-08-21 01:14:27 +0530

    Added data_types.py file

---
+++ 02-python/data_types.py
@@ -0,0 +1,13 @@
+customer_name="Anu"
+age=22
+salary=25000
+is_married=False
+
+print("Customer:", customer_name)
+print("Customer Type:", type(customer_name))
+print("Age:", age)
+print("Age Type:", type(age))
+print("Salary:", salary)
+print("Salary Type:", type(salary))
+print("Is Married:", is_married)
+print("Is Married Type:", type(is_married))
